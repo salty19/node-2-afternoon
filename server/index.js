eecc933934955aa0670c6de2cdfde8adba140942
Kara LeSueur <kara.lesueur@gmail.com>
2020-09-02 20:53:03 -0700

    nevermind - it wasnt broken

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const mc = require('./controllers/messages_controller')
+const messagesController = require('./controllers/messages_controller')
 
 const app = express()
 const port = 3001
